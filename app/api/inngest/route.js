ab0e5fb4580b991f5d6b4793b2d1a8e6ff586529
zainkhan <khanzain33999@gmail.com>
2025-06-21 22:32:40 +0500

    5

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,14 +1,14 @@
      
      import { serve } from "inngest/next";
-     import { inngest, syncUserCreation, syncUserDeletion } from "../../../config/inggest";
+     import { inngest, syncUserCreation,syncUserUpdation ,} from "../../../config/inggest";
      
      
      export const { GET, POST, PUT } = serve({
        client: inngest,
        functions: [
         syncUserCreation,
-        syncUserDeletion,
-        syncUserDeletion,   
+        syncUserUpdation,
+      
        ],
      });
         
