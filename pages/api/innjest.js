8fc2e34ded29b51dd132034b9d05ccaa1b46caa1
zainkhan <khanzain33999@gmail.com>
2025-06-23 16:28:51 +0500

    [6]

---
+++ pages/api/innjest.js
@@ -0,0 +1,14 @@
+      
+      import { serve } from "inngest/next";
+      import { inngest, syncUserCreation,syncUserUpdation ,} from "../../config/inggest"
+      
+      
+      export const { GET, POST, PUT } = serve({
+        client: inngest,
+        functions: [
+          syncUserCreation,
+          syncUserUpdation,
+        
+        ],
+      });
+          
