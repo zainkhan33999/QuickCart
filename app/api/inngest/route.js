b5f2c30184f90e0f0d3d23b72d7196023f53a9c1
zainkhan <khanzain33999@gmail.com>
2025-06-23 16:47:27 +0500

    [7]

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
       
       import { serve } from "inngest/next";
-      import { inngest, syncUserCreation,syncUserUpdation ,} from "../../config/inngest"
+      import { inngest, syncUserCreation,syncUserUpdation ,} from "../../../config/inngest"
       
       
       export const { GET, POST, PUT } = serve({
