dfa1c0c878187aafecd1b3eecf6e72b816079654
zainkhan <khanzain33999@gmail.com>
2025-06-24 12:34:42 +0500

    fixed another bug

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,6 +10,7 @@ import { useClerk, UserButton } from "@clerk/nextjs";
 const Navbar = () => {
 
   const { isSeller, router,user } = useAppContext();
+  console.log(user,"user")
 const {openSignIn} = useClerk()
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
@@ -40,7 +41,7 @@ const {openSignIn} = useClerk()
       <ul className="hidden    md:flex items-center gap-4 ">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
    { 
-   user
+   user.isSignedIn
    ? <>
    <UserButton>
    <UserButton.MenuItems>
@@ -56,7 +57,7 @@ const {openSignIn} = useClerk()
    </UserButton.MenuItems>
    </UserButton>
    </>
-   : <button onClick={openSignIn} className="flex items-center gap-2 hover:text-gray-900 transition">
+   :   <button onClick={openSignIn} className="flex items-center gap-2 hover:text-gray-900 transition">
           <Image src={assets.user_icon} alt="user icon" />
           Account
         </button>
